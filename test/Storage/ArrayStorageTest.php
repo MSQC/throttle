335f42beab2ebb3a0ebf272d05056d5d56af61dc
Bernd Rathmanner <bernd.rathmanner@laola1.at>
2016-02-11 20:28:04 +0100

    extracts most the tests into a custom abstract class

--- test/Storage/ArrayStorageTest.php
+++ test/Storage/ArrayStorageTest.php
@@ -1,56 +1,13 @@
 <?php
 namespace sideshow_bob\throttle\Storage;
 
-use sideshow_bob\throttle\StorageInterface;
-
-class ArrayStorageTest extends \PHPUnit_Framework_TestCase
+class ArrayStorageTest extends AbstractStorageTest
 {
-    /** @var  StorageInterface */
-    private $storage;
-
     /**
      * @inheritdoc
      */
-    protected function setUp()
+    protected function createStorage()
     {
-        $this->storage = new ArrayStorage();
-        $this->storage->save("identifier", 1);
-    }
-
-    public function testInvalidIdentifier()
-    {
-        $this->setExpectedException("InvalidArgumentException");
-        $this->storage->get(new \stdClass());
-    }
-
-    public function testMissingData()
-    {
-        $this->assertEquals(0, $this->storage->get("non-existing-identifier"));
-    }
-
-    public function testDataExpiration()
-    {
-        $this->storage->save("identifier", 1, 1);
-        sleep(2);
-        $this->assertEquals(0, $this->storage->get("non-existing-identifier"));
-    }
-
-    public function testSaveAndGet()
-    {
-        $this->assertEquals(1, $this->storage->get("identifier"));
-    }
-
-    public function testSaveAndIncrement()
-    {
-        $this->assertEquals(2, $this->storage->increment("identifier"));
-    }
-
-    /**
-     * @after testSaveAndGet
-     */
-    public function testSaveAndDelete()
-    {
-        $this->storage->delete("identifier");
-        $this->assertEquals(0, $this->storage->get("identifier"));
+        return new ArrayStorage();
     }
 }
