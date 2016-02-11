0eea86715a673b0c20139b0d8d9c3c694319aa5d
Bernd Rathmanner <bernd.rathmanner@laola1.at>
2016-02-11 20:23:02 +0100

    updates StorageInterface definition

--- src/StorageInterface.php
+++ src/StorageInterface.php
@@ -2,52 +2,36 @@
 namespace sideshow_bob\throttle;
 
 /**
- * StorageInterface
- * Interface defining methods that must be implemented in the storage classes.
- *
- * @package Websoftwares
- * @license http://www.dbad-license.org/ DbaD
- * @version 0.3.3
- * @author boris <boris@websoftwar.es>
+ * StorageInterface defining methods to track the data of a different requests.
+ * @package sideshow_bob\throttle
  */
 interface StorageInterface
 {
     /**
-     * save
-     *
-     * @param mixed $identifier identifier to save
-     * @param int   $amount     the current amount
-     * @param int   $timespan   the timespan in seconds
-    */
-    public function save($identifier, $amount, $timespan);
+     * Get the current amount of request for a specific identifier.
+     * @param string $identifier
+     * @return int
+     */
+    public function get($identifier);
 
     /**
-     * update
-     *
-     * @param mixed $identifier identifier to update
-     * @param int   $amount     the current amount
-     * @param int   $timespan   the timespan in seconds
-    */
-    public function update($identifier, $amount, $timespan);
+     * Save the current amount of requests for a specific identifier.
+     * @param string $identifier identifier to save
+     * @param int $amount the current amount
+     * @param int $ttl [optional] time to live in seconds
+     */
+    public function save($identifier, $amount, $ttl = 300);
 
     /**
-     * increment
-     *
-     * @param mixed $identifier identifier to increment the value for
+     * Increment the amount of a specific identifier and return it's new amount.
+     * @param string $identifier
+     * @return int
      */
     public function increment($identifier);
 
     /**
-     * delete
-     *
-     * @param mixed $identifier identifier to delete the entry for
+     * Remove a specific identifier.
+     * @param string $identifier
      */
     public function delete($identifier);
-
-    /**
-     * get
-     *
-     * @param mixed $identifier identifier to retrieve the value from storage
-     */
-    public function get($identifier);
 }
