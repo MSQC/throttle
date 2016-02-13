1cd87c60460bf22fc8f1ea16f9dd5ab60dd944b2
Bernd Rathmanner <bernd.rathmanner@laola1.at>
2016-02-13 17:21:59 +0100

    extracts all installations into .travis.install.sh

--- .travis.install.sh
+++ .travis.install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# composer update
+composer self-update
+composer install --no-interaction --prefer-source
+
 # enable php modules
 if [ -f ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini ]; then
     echo "extension = memcached.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
